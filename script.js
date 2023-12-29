cec00cd5eaf0b286cb5f369904821c4341d18697
Serhii <sergkraken2003@gmail.com>
2023-12-29 18:01:54 +0200

    29122023 commit added, Practice part 2 ended

--- script.js
+++ script.js
@@ -8,12 +8,25 @@ const  personalMovieDB = {
         private: false
 };
 
-const a = prompt("Один з останніх переглянутих фільмів?", ''),
-    b = prompt("На скільки оціните його?", ''),
-    c = prompt("Один з останніх переглянутих фільмів?", ''),
-    d = prompt("На скільки оціните його?", '');
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
+    for (let i = 0; i < 2; i++) { 
+        const a = prompt("Один з останніх переглянутих фільмів?", ''),
+        b = prompt("На скільки оціните його?", '');
+        if (a != null && b != null && a != '' && b != '' && a.length < 50) { 
+            personalMovieDB.movies[a] = b;
+            console.log('Done');
+        } else { 
+            console.log('Error');
+            i--;
+        }
+        
+    }
+if (personalMovieDB.count < 10) { 
+    console.log('Ви переглянули мало фільмів');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) { 
+    console.log('Ви классик');
+} else if (personalMovieDB.count >= 30) { 
+    console.log('Ви кіноман');
+} else { 
+    console.log("Помилка");
+}
 console.log(personalMovieDB);
