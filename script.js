d992f2d25b29b564d170a9febb6e933c846b4b9b
Serhii <sergkraken2003@gmail.com>
2024-01-01 11:09:15 +0200

    01.01.2024 Practice Part 3 ended

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
-const numberOfFilms = +prompt("Скільки фільмів ви вже переглянули?", "");
 
+let numberOfFilms;
+
+function start () {
+    numberOfFilms = +prompt("Скільки фільмів ви вже переглянули?", "");
+    
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) { 
+        numberOfFilms = +prompt("Скільки фільмів ви вже переглянули?", "");
+    }
+}
+
+start();
 const  personalMovieDB = { 
         count: numberOfFilms,
         movies: {},
@@ -8,6 +18,9 @@ const  personalMovieDB = {
         private: false
 };
 
+    
+
+function rememberMyFilms () { 
     for (let i = 0; i < 2; i++) { 
         const a = prompt("Один з останніх переглянутих фільмів?", ''),
         b = prompt("На скільки оціните його?", '');
@@ -20,13 +33,37 @@ const  personalMovieDB = {
         }
         
     }
-if (personalMovieDB.count < 10) { 
-    console.log('Ви переглянули мало фільмів');
-} else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) { 
-    console.log('Ви классик');
-} else if (personalMovieDB.count >= 30) { 
-    console.log('Ви кіноман');
-} else { 
-    console.log("Помилка");
 }
-console.log(personalMovieDB);
+
+rememberMyFilms();
+
+
+function detectLevel () { 
+    if (personalMovieDB.count < 10) { 
+        console.log('Ви переглянули мало фільмів');
+    } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) { 
+        console.log('Ви классик');
+    } else if (personalMovieDB.count >= 30) { 
+        console.log('Ви кіноман');
+    } else { 
+        console.log("Помилка");
+    }
+}
+
+detectLevel();
+
+function showMyDB (hidden) {
+    if (!hidden) {
+        console.log(personalMovieDB);
+    }
+}
+
+showMyDB(personalMovieDB.private);
+
+function writeYourGenres() { 
+    for (let i = 1; i <= 3; i++) {
+        personalMovieDB.genres[i - 1] = prompt(`Ваш улюблений жанр за номером ${i}`);   
+    }
+}
+
+writeYourGenres();
